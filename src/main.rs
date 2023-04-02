0e4428e700d157b5bee9f109339bea1987f859ef
Tony Fleck <tonyfleck76@gmail.com>
2023-04-01 22:59:41 -0400

    Bundling assets in the binaries, hopefully have builds working?

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod game_over;
 mod global;
 mod main_menu;
 
+use bevy_embedded_assets::EmbeddedAssetPlugin;
 use game::event::*;
 use game::plugin::GamePlugin;
 use game_over::plugin::GameOverPlugin;
@@ -18,14 +19,19 @@ const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
 fn main() {
     App::new()
         // Default and window setup
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Asteroids!".into(),
-                resolution: (800., 800.).into(),
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Asteroids!".into(),
+                        resolution: (800., 800.).into(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build()
+                .add_before::<bevy::asset::AssetPlugin, _>(EmbeddedAssetPlugin),
+        )
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         // State
         .add_state::<AppState>()
